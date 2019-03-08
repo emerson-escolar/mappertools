85cd9ea341ccbaef279494b1b2eb4f36f4fc8c58
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2019-03-08 18:52:26 +0900

    add more testing for uniform cover

--- mappertools/tests/test_covers.py
+++ mappertools/tests/test_covers.py
@@ -12,3 +12,17 @@ def test_uniform_overlapped():
     lb, ub = cv.uniform_cover_fences(0,10,2,0.75)
     assert np.all(lb == np.array([0,2]))
     assert np.all(ub == np.array([8,10]))
+
+def test_uniform_lengths_overlaps():
+    p = 0.42
+    n = 13
+    lb, ub = cv.uniform_cover_fences(0,100, n, p)
+    assert len(ub) == n == len(lb)
+
+    lengths = ub - lb
+    assert np.allclose(lengths, lengths[0])
+
+    # check that actual overlap is correct
+    for i in range(len(lb)-1):
+        assert np.isclose( (ub[i] - lb[i+1]), p * (ub[i]-lb[i]))
+        assert np.isclose( (ub[i] - lb[i+1]), p * (ub[i+1]-lb[i+1]))
