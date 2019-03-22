eba8eb35d294ff9ac149a91d3ef253987ba31a97
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2019-03-22 13:27:47 +0900

    test flare detection on pathgraph

--- mappertools/tests/test_flares.py
+++ mappertools/tests/test_flares.py
@@ -1,5 +1,6 @@
 import pytest
 import mappertools.features.flares as flr
+import networkx as nx
 
 def test_flare():
     x = flr.Flare(0,10)
@@ -8,3 +9,18 @@ def test_flare():
     assert x.finished == True
     assert 0 in x.nodes
     assert 1 not in x.nodes
+
+
+def test_detection():
+    G = nx.generators.classic.path_graph(10)
+
+    # trivial 'centrality'
+    cen = {n:1 for n in G.nodes}
+    flares = flr.flare_detect(G,cen)
+    assert len(flares) == 1
+    assert len(flares[0].nodes) == 10
+
+    # harmonic centrality
+    cen = nx.centrality.harmonic_centrality(G)
+    flares = flr.flare_detect(G,cen)
+    assert len(flares) == 2
