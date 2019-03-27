733adbad6b0cda20c5b4ff699c46119dac3ab49f
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2019-03-27 16:33:44 +0900

    add test for precomputed metric to linkagemapper

--- mappertools/tests/test_linkage_mapper.py
+++ mappertools/tests/test_linkage_mapper.py
@@ -1,5 +1,6 @@
 import pytest
 import scipy.cluster.hierarchy
+import scipy.spatial.distance as spd
 import numpy as np
 
 import mappertools.linkage_mapper as lk
@@ -36,4 +37,27 @@ def test_heuristics():
     assert len(np.unique(fg.labels_)) == 3
 
     sil = lk.LinkageMapper(heuristic='sil').fit(X)
+    assert len(np.unique(sil.labels_)) == 3
+
+
+def test_heuristics_precomputed():
+    # artificial data with "obvious" clustering
+    X = np.array([[0,0,0],
+                  [0,1,0],
+                  [1,0,0],
+                  [0,0,1],
+                  [100,0,0],
+                  [100,1,0],
+                  [101,0,0],
+                  [100,0,1],
+                  [0,0,100],
+                  [0,1,100],
+                  [1,0,100],
+                  [0,0,101]])
+    dists = spd.pdist(X)
+
+    fg = lk.LinkageMapper(heuristic='firstgap', metric='precomputed').fit(dists)
+    assert len(np.unique(fg.labels_)) == 3
+
+    sil = lk.LinkageMapper(heuristic='sil', metric='precomputed').fit(dists)
     assert len(np.unique(sil.labels_)) == 3
