2c0971b48b8c4a163885220ea03f4085ccca98f5
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2019-06-07 19:06:44 +0900

    refactor flare_balls, add some documentation

--- mappertools/features/flare_balls.py
+++ mappertools/features/flare_balls.py
@@ -1,11 +1,20 @@
 import networkx as nx
 import numpy as np
 
+import pandas
+
 def get_nodes_containing_member(G, member, query_data='unique_members'):
     return (node for node in G if member in G.nodes[node][query_data])
 
 
 def compute_core_shell(G, H):
+    """
+    Compute core-shell decomposition of a subset of nodes H with respect to graph G.
+
+    A vertex x in H is said to be core if all neighbors of x are in H.
+    Otherwise, it is said to be shell
+    """
+
     core = []
     shell = []
     for x in H:
@@ -27,8 +36,8 @@ def compute_flareness(G, member,
     core, shell = compute_core_shell(G, G_member)
 
     if len(G_member) == 0:
-        print("member {} not found. Ignoring".format(member))
-        return
+        print("Member {} not found. Ignoring".format(member))
+        return None, None
 
     if verbose > 0:
         print("G_member: ", G_member)
@@ -41,16 +50,35 @@ def compute_flareness(G, member,
         distances = nx.multi_source_dijkstra_path_length(G_member_subgraph,shell,
                                                          weight=weight)
     k = []
-    for L in nx.connected_components(G.subgraph(core)):
-        k_L = -np.inf
-        for x in L:
-            if x in distances and distances[x] > k_L:
-                k_L = distances[x]
-            elif x not in distances:
-                k_L = np.inf
-                break
+    components = list(nx.connected_components(G.subgraph(core)))
+    for L in components:
+        k_L = max(distances.get(x,np.inf) for x in L)
         k.append(k_L)
-    return k
+
+    return (k, components)
+
+
+
+
+def compute_all_summary(G, members, weight=(lambda v,u,e: 1), query_data='unique_members'):
+    ans = pandas.DataFrame(columns=['type','k_C','k_vec'],index=members)
+    for member in members:
+        k, _ = compute_flareness(G, member, weight, query_data)
+        if k is None:
+            ans.loc[member] = pandas.Series({'type':-1, 'k_C':None, 'k_vec':None})
+        elif len(k) == 0:
+            ans.loc[member] = pandas.Series({'type':0, 'k_C':0, 'k_vec':k})
+        else:
+            if is_pure_island(k):
+                ans.loc[member] = pandas.Series({'type':3, 'k_C':np.inf, 'k_vec':k})
+            elif has_flare(k):
+                k_C = max(np.array(k)[np.array(k) != np.inf])
+                if has_island(k):
+                    ans.loc[member] = pandas.Series({'type':2, 'k_C':k_C, 'k_vec':k})
+                else:
+                    ans.loc[member] = pandas.Series({'type':1, 'k_C':k_C, 'k_vec':k})
+    return ans
+
 
 
 def compute_all_flareness(G, members, weight=(lambda v,u,e: 1), query_data='unique_members'):
@@ -58,7 +86,7 @@ def compute_all_flareness(G, members, weight=(lambda v,u,e: 1), query_data='uniq
     not_flare_nor_island = []
     flareness = {}
     for member in members:
-        k = compute_flareness(G, member, weight, query_data)
+        k, _ = compute_flareness(G, member, weight, query_data)
         if k is None:
             not_found.append(member)
         elif len(k) == 0:
