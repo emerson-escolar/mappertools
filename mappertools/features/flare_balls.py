6e054edd0c1d1d871df7322fca51f685eba76c46
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2019-06-07 14:06:07 +0900

    fix bug, add reporting to flare balls

--- mappertools/features/flare_balls.py
+++ mappertools/features/flare_balls.py
@@ -22,7 +22,7 @@ def compute_member_core_shell(G, member, query_data='unique_members'):
     return G_member, member_core, member_shell
 
 
-def compute_flareness(G, member, weight=(lambda v,u,e: 1), query_data='unique_members'):
+def compute_flareness(G, member, weight=(lambda v,u,e: 1), query_data='unique_members',verbose=0):
     G_member_nodes, core, shell = compute_member_core_shell(G, member, query_data)
 
     if len(G_member_nodes) == 0:
@@ -30,14 +30,18 @@ def compute_flareness(G, member, weight=(lambda v,u,e: 1), query_data='unique_me
         return
     
     # assert(G_member_nodes == set(core).union(shell))
-    # print("G_member_nodes: ", G_member_nodes)
-    # print("core: ", core)
-    # print("shell: ", shell)
+    if verbose > 0:
+        print("G_member_nodes: ", G_member_nodes)
+        print("core: ", core)
+        print("shell: ", shell)
 
     G_member = G.subgraph(G_member_nodes)
     distances = {}
-    if len(shell) > 1:
+    if len(shell) > 0:
         distances = nx.multi_source_dijkstra_path_length(G_member, shell,weight=weight)
+
+    if verbose > 0:
+        print(distances)
 
     k = []
     for L in nx.connected_components(G.subgraph(core)):
@@ -50,3 +54,72 @@ def compute_flareness(G, member, weight=(lambda v,u,e: 1), query_data='unique_me
                 break
         k.append(k_L)
     return k
+
+
+def compute_all_flareness(G, members, weight=(lambda v,u,e: 1), query_data='unique_members'):
+    not_found = []
+    not_flare_nor_island = []
+    flareness = {}
+    for member in members:
+        k = compute_flareness(G, member, weight, query_data)
+        if k is None:
+            not_found.append(member)
+        elif len(k) == 0:
+            not_flare_nor_island.append(member)
+        else:
+            flareness[member] = k
+
+    return (flareness, not_flare_nor_island, not_found)
+
+
+def has_island(k):
+    return np.any(np.array(k) == np.inf)
+
+def has_flare(k):
+    return np.any(np.array(k) != np.inf)
+
+def is_pure_island(k):
+    return len(k) > 0 and np.alltrue(np.array(k) == np.inf)
+
+
+def flareness_report(G, members, weight=(lambda v,u,e: 1), query_data='unique_members'):
+    flareness, not_flare_nor_island, not_found = compute_all_flareness(G,members,weight,query_data)
+
+    pure_island = []
+    both = []
+    flare = []
+    for firm, k in flareness.items():
+        if is_pure_island(k):
+            pure_island.append(firm)
+        elif has_flare(k):
+            if has_island(k):
+                both.append(firm)
+            else:
+                flare.append(firm)
+
+    flare = sorted(flare, key=lambda x:max(flareness[x]))
+
+    print("***PURE ISLAND***")
+    for x in pure_island:
+        print(x)
+    print()
+
+    print("***FLARE AND ISLAND***")
+    for x in both:
+        print(x, flareness[x])
+    print()
+
+    print("***FLARE ONLY***")
+    for x in flare:
+        print(x, flareness[x])
+    print()
+
+    print("***NOT FLARE NOR ISLAND***")
+    for x in not_flare_nor_island:
+        print(x)
+    print()
+
+
+    print("***NOT FOUND***")
+    for x in not_found:
+        print(x)
