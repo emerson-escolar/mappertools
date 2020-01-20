d92bd7ba46048361a0c7cdcff3efa85ee4d1c473
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2020-01-20 16:33:41 +0900

    closeness to dissimilarity

--- mappertools/distances.py
+++ mappertools/distances.py
@@ -44,5 +44,56 @@ def normalize_diagonal_to_one(A):
 
     L = np.diag(np.sqrt(1./np.diag(A)))
     normed_A = L @ A @ L
+    np.fill_diagonal(normed_A, 1)
 
     return normed_A
+
+
+def flipped_bloom_mahalanobis_distance(X):
+    """
+    Compute a distance based on
+    Mahalanobis normed technology closeness measure
+    as defined in Bloom, Schankerman, Van Reenen (2013)
+
+    Parameters
+    ----------
+    X : array [n_samples, n_features]
+        data as a feature array.
+
+    Returns
+    -------
+    dissimilarity : array [n_samples, n_samples]
+        distance matrix
+    """
+
+    closeness = bloom_mahalanobis_closeness(X)
+    dissimilarity = np.max(closeness) - closeness
+    np.fill_diagonal(dissimilarity, 0)
+    assert np.allclose(dissimilarity, dissimilarity.T)
+
+    return (dissimilarity + dissimilarity.T)/2
+
+
+
+def normalized_bloom_mahalanobis_distance(X):
+    """
+    Compute a distance based on
+    Mahalanobis normed technology closeness measure
+    as defined in Bloom, Schankerman, Van Reenen (2013)
+
+    Parameters
+    ----------
+    X : array [n_samples, n_features]
+        data as a feature array.
+
+    Returns
+    -------
+    dissimilarity : array [n_samples, n_samples]
+        distance matrix
+    """
+
+    closeness = bloom_mahalanobis_closeness(X)
+    dissimilarity = 1 - normalize_diagonal_to_one(closeness)
+    assert np.allclose(dissimilarity, dissimilarity.T)
+
+    return (dissimilarity + dissimilarity.T)/2
