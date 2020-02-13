89428387edbced7d6d890457bf9f0b978375d090
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2020-02-13 10:58:02 +0900

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 
 setup(name='mappertools',
       author='Emerson G. Escolar',
-      version='0.3.2',
+      version='0.3.3',
       description='Mapper tools',
       packages=find_packages(exclude=["*.tests"]),
       install_requires=['numpy', 'matplotlib']
