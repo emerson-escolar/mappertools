b8ebb78b52b0e17fda2d150edadd143a7daaaa8a
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2020-03-18 14:07:46 +0900

    start work on kMedoids clustering

---
+++ mappertools/mapper/clustering.py
@@ -0,0 +1,99 @@
+import math
+import numpy as np
+
+import sklearn.base
+
+import scipy.spatial.distance as ssd
+
+import pyclustering.cluster.kmedoids as kmedoids
+import pyclustering.cluster.center_initializer as pci
+import pyclustering.utils.metric as pcm
+
+pyclustering_metrics = {"euclidean": pcm.type_metric.EUCLIDEAN,
+                        "euclidean_square": pcm.type_metric.EUCLIDEAN_SQUARE,
+                        "manhattan": pcm.type_metric.MANHATTAN,
+                        "chebyshev": pcm.type_metric.CHEBYSHEV,
+                        "minkowski": pcm.type_metric.MINKOWSKI,
+                        "canberra": pcm.type_metric.CANBERRA,
+                        "chi_square": pcm.type_metric.CHI_SQUARE,
+                        "gower": pcm.type_metric.GOWER}
+scipy_metrics = {"braycurtis": ssd.braycurtis,
+                 "correlation": ssd.correlation,
+                 "cosine": ssd.cosine,
+                 "jensenshannon": ssd.jensenshannon}
+
+
+def _clusters_to_labels(clusters, prefix=None):
+    n_clusters = len(clusters)
+    if n_clusters == 0: return []
+
+    fstr = prefix + "_" if prefix else ""
+    fstr += ("{:0" + str(math.ceil(math.log(n_clusters,10))) + "d}")
+
+    n_points = sum([len(cluster) for cluster in clusters])
+    labels = ["none"] * n_points
+    for i, cluster in enumerate(clusters):
+        cluster_str = fstr.format(i)
+        for point in cluster:
+            labels[point] = cluster_str
+
+    return labels
+
+
+def _process_metric(metric):
+    if metric == "precomputed":
+        return None
+
+    if metric in pyclustering_metrics:
+        pcc_type_metric = pyclustering_metrics[metric]
+        return pcm.distance_metric(pcc_type_metric)
+    if callable(metric):
+        pcc_type_metric = pcm.type_metric.USER_DEFINED
+        metric_func = metric
+    elif metric in scipy_metrics:
+        pcc_type_metric = pcm.type_metric.USER_DEFINED
+        metric_func = scipy_metrics[metric]
+    else:
+        raise RuntimeError("Metric {} not recognized".format(str(metric)))
+
+    pcc_metric = pcm.distance_metric(pcc_type_metric, func = metric_func)
+    return pcc_metric
+
+
+
+class kMedoids(sklearn.base.BaseEstimator,
+               sklearn.base.ClusterMixin):
+    def __init__(self, metric, heuristic,
+                 k_max=None, prefix="kMedoids", verbose=1):
+        self.metric = metric
+        self.pcc_metric = _process_metric(metric)
+
+        self.heuristic = heuristic
+        self.verbose = verbose
+
+        self.prefix = prefix
+
+        if k_max == None:
+            k_max = np.inf
+        self.k_max = k_max
+
+
+    def _fit_k(self, X, k):
+        if hasattr(X, "to_numpy") and callable(X.to_numpy):
+            X = X.to_numpy()
+        initial_medoids = pci.random_center_initializer(X, k).initialize(return_index=True)
+
+        if self.metric == "precomputed":
+            ans = kmedoids.kmedoids(X, initial_medoids,
+                                        data_type = "distance_matrix")
+        else:
+            ans = kmedoids.kmedoids(X, initial_medoids, metric=self.pcc_metric)
+        ans.process()
+        self.labels_ = _clusters_to_labels(ans.get_clusters(), prefix=self.prefix)
+
+        return self
+
+
+    def fit(self, X, y=None):
+        if isinstance(self.heuristic, int):
+            return self._fit_k(X, self.heuristic)
