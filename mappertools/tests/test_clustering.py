bad0d88ed1aa2d85f1d0ad0cb7e7ff16adced24c
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2020-04-08 18:28:38 +0900

    add test for kMedoids

---
+++ mappertools/tests/test_clustering.py
@@ -0,0 +1,15 @@
+import pytest
+import scipy.cluster.hierarchy
+import scipy.spatial.distance as spd
+import numpy as np
+
+import mappertools.mapper.clustering as mclust
+
+def test_two_clustering():
+    X = np.random.rand(100,3)
+    Y = np.random.rand(100,3) + np.array([[10,5,1]])
+    data = np.concatenate((X,Y),axis=0)
+    labels = mclust.kMedoids(metric="euclidean", heuristic=2, prefix=None).fit(data).labels_
+    for i in range(100):
+        assert labels[i] == labels[0]
+        assert labels[i+100] == labels[100]
