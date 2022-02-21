4db10c0a1c0e467278c4046d95902a9c08cef36e
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2022-02-21 14:54:08 +0900

    v0.3.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 
 setup(name='mappertools',
       author='Emerson G. Escolar',
-      version='0.3.6',
+      version='0.3.7',
       description='Mapper tools',
       packages=find_packages(exclude=["*.tests"]),
       install_requires=['numpy', 'matplotlib', 'pyclustering', 'networkx', 'pandas',
